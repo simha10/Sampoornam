6382f0a7f8b30afdfda575c067ee51d96ae2c47c
simha10 <simhachalammandapudi@gmail.com>
2026-03-09 18:50:41 +0530

    addfrontend url to backend cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:7000", "http://localhost:3000"],
+    origin: ["http://localhost:7000", "http://localhost:3000", 'https://sampoornam.vercel.app'],
     credentials: true,
 }));
 app.use(express.json());
